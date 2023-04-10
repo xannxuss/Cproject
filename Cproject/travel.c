45eb221f537795ec5f8c007ceb858af6597ec179
xannxuss <118335528+xannxuss@users.noreply.github.com>
2023-04-10 14:15:45 +0800

    Update travel.c

--- Cproject/travel.c
+++ Cproject/travel.c
@@ -10,7 +10,13 @@
 #include "simpio.h"
 #include "strlib.h"
 
-
+typedef struct tourism/*旅游项目结构体*/
+{
+char num[10];  /*项目编号*/
+char name[10];  /*旅游项目名称*/
+char place[10]; /*地点*/
+int money;     /*金额*/   
+}Tourism;
 
 
 
